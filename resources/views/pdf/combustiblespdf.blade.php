a153518e2ca95a3eedf3c24d4316075366e86657
Raúl Trigueros <rtrigueros@AMSP.TACACHICO>
2022-10-06 10:07:09 -0600

    Cambio de imagen de encabezado pdf

--- resources/views/pdf/combustiblespdf.blade.php
+++ resources/views/pdf/combustiblespdf.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <center>
-    <img class="navbar-brand-full" src="img/encabezado.png" width="700" height="75px" alt="encabezado" text-align:center>
+    <img class="navbar-brand-full" src="img/encabezado.jpg" width="700" height="75px" alt="encabezado" text-align:center>
     </center>
     <title>Reporte de Consumo de combustible</title>
     <style>
